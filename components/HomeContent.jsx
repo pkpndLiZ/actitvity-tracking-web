1518744d926f41e22e84f2085bfaa70855946100
pkpndLiZ <phanida.lip@gmail.com>
2023-05-01 14:37:40 +0700

    feat: relocate cardItem to HomeContent

--- components/HomeContent.jsx
+++ components/HomeContent.jsx
@@ -1,3 +1,19 @@
+import { CardItem } from "@/components/CardItem";
+
 export function HomeContent() {
-  return <p>Home Content</p>;
+  return (
+    <>
+      <CardItem
+        profileName="Abdula.raf"
+        profileImage="/images/mock/man.png"
+        // imageUrl="/images/mock/man.png"
+        imageUrl={null}
+        duration={{ hr: 4, min: 23 }}
+        distance="4.6"
+        date="07/03/2030"
+        title="We Can Do It!"
+        description="View this so good."
+      />
+    </>
+  );
 }
