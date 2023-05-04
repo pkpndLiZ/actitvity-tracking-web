56acd15cb5cdf60d6467364ab56ae59da496c81b
pumchanpom <pum.chanpom@gmail.com>
2023-05-04 16:40:36 +0700

    feat:implement useForm in Login Page.

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,48 @@
-export default function Login() {
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export const Login = (props) => {
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (loginData) => {
+    console.log("email: ", loginData.email);
+    console.log("password: ", loginData.password);
+  };
+
   return (
-    <>
-      <p>Login Page</p>
-    </>
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">email</label>
+        <input
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          {...register("email")}
+          id="email"
+          type="email"
+          name="email"
+          placeholder="youremail@gmail.com"
+        />
+        <input
+          // value={passw
+          // onChange={(e) => setPassword(e.target.value)}
+          {...register("password", { required: true })}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
   );
-}
+};
