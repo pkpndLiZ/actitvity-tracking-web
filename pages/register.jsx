c06e09c31f69c0d5adff619d1ba724491eed029c
pumchanpom <pum.chanpom@gmail.com>
2023-05-06 19:25:28 +0700

    feat: change typo and styling of texts and colors

--- pages/register.jsx
+++ pages/register.jsx
@@ -2,12 +2,6 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const Register = (props) => {
-  // const [email, setEmail] = useState("");
-  // const [pass, setPass] = useState("");
-  // const [name, setName] = useState("");
-  // const [birthdate, setBirthdate] = useState("");
-  // const [gender, setGender] = useState("");
-
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (registerData) => {
@@ -19,67 +13,54 @@ export const Register = (props) => {
   };
 
   return (
-    <div className="auth-form-container">
-      <h2>Register</h2>
-      <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
-        {/* name */}
-        <label htmlFor="name">Name</label>
-        <input
-          // value={name}
-          // name="name"
-          // onChange={(e) => setName(e.target.value)}
-          {...register("name")}
-          id="name"
-          placeholder="name"
-        />
-        {/* birthdate */}
-        <label htmlFor="birthdate">Birthdate</label>
-        <input
-          // value={birthdate}
-          // name="birthdate"
-          // onChange={(e) => setBirthdate(e.target.value)}
-          {...register("birthdate")}
-          id="birthdate"
-          placeholder="birthdate"
-          type="date"
-        />
-        {/* gender */}
-        <label htmlFor="gender">Gender</label>
-        <input
-          // value={gender}
-          // name="gender"
-          // onChange={(e) => setGender(e.target.value)}
-          {...register("gender")}
-          id="gender"
-          placeholder="gender"
-        />
-        {/* email */}
-        <label htmlFor="email">Email</label>
-        <input
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          {...register("email")}
-          type="email"
-          placeholder="email@email.com"
-          id="email"
-          // name="email"
-        />
-        {/* password */}
-        <label htmlFor="password">Password</label>
-        <input
-          // value={password}
-          // onChange={(e) => setPass(e.target.value)}
-          type="password"
-          placeholder="********"
-          id="password"
-          {...register("password")}
-          // name="password"
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
-        Already have an account? Login here.
-      </button>
+    <div className="App">
+      <div className="auth-form-container">
+        <div className="title">Register</div>
+        <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="name">Name</label>
+          <input
+            {...register("name")}
+            id="name"
+            placeholder="name"
+            type="text"
+          />
+          <label htmlFor="birthdate">Birthdate</label>
+          <input
+            {...register("birthdate")}
+            id="birthdate"
+            placeholder="birthdate"
+            type="date"
+          />
+          <label htmlFor="gender">Gender</label>
+          <input
+            {...register("gender")}
+            id="gender"
+            placeholder="gender"
+            type="text"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email")}
+            placeholder="email@email.com"
+            id="email"
+            type="email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password")}
+            id="password"
+            placeholder="********"
+            type="password"
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <button
+          className="link-btn"
+          onClick={() => props.onFormSwitch("login")}
+        >
+          Already have an account? Login here.
+        </button>
+      </div>
     </div>
   );
 };
