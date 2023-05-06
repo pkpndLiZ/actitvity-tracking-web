bac6e4f75bae67a48a16d79247cab403590ac95b
pumchanpom <pum.chanpom@gmail.com>
2023-05-06 16:21:43 +0700

    fix: solve connection between login.jsx and css

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const Login = (props) => {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (loginData) => {
@@ -13,36 +10,35 @@ export const Login = (props) => {
   };
 
   return (
-    <div className="auth-form-container">
-      <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">email</label>
-        <input
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          {...register("email")}
-          id="email"
-          type="email"
-          name="email"
-          placeholder="youremail@gmail.com"
-        />
-        <input
-          // value={passw
-          // onChange={(e) => setPassword(e.target.value)}
-          {...register("password", { required: true })}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        />
-        <button type="submit">Log In</button>
-      </form>
-      <button
-        className="link-btn"
-        onClick={() => props.onFormSwitch("register")}
-      >
-        Don't have an account? Register here.
-      </button>
+    <div className="App">
+      <div className="auth-form-container">
+        <h1>Login</h1>
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email")}
+            id="email"
+            type="email"
+            name="email"
+            placeholder="youremail@gmail.com"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <button type="submit">Log In</button>
+        </form>
+        <button
+          className="link-btn"
+          onClick={() => props.onFormSwitch("register")}
+        >
+          Don't have an account? Register here.
+        </button>
+      </div>
     </div>
   );
 };
