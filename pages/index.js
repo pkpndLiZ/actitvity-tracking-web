3525f30322a287eaeb223679393e968f70d23972
pumchanpom <pum.chanpom@gmail.com>
2023-05-06 16:26:20 +0700

    feat: add Login to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import { MainLayout } from "@/components/layouts/MainLayout";
 import { HomeContent } from "@/components/HomeContent";
+import { Login } from "./login";
 
 export default function Home() {
   return (
     <>
-      <MainLayout>
-        <HomeContent />
-      </MainLayout>
+      <Login />
     </>
   );
 }
