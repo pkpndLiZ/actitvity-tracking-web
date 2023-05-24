9e9b4f065715135ec96712fcb167effeb980ab62
pkpndLiZ <phanida.lip@gmail.com>
2023-05-24 16:26:33 +0700

    fix conflict code

--- src/userContext.js
+++ src/userContext.js
@@ -2,27 +2,34 @@
 import React, { createContext, useState, useEffect } from "react";
 import { fetch } from "./axiosInstance";
 import useSWR from "swr";
-
+import { useSWRConfig } from "swr";
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   const [userId, setUserId] = useState(null);
-
+  const { cache } = useSWRConfig();
   const { data: userData, mutate: updateUserData } = useSWR(
     userId ? "/api/users/" + userId : null,
     fetch
   );
 
   useEffect(() => {
-    const userId = localStorage.getItem("userId");
-    if (userId) {
-      setUserId(userId);
+    console.log("userId:", userId);
+    const userIdCache = localStorage.getItem("userId");
+    console.log("userIdCache:", userIdCache);
+    if (userIdCache) {
+      setUserId(userIdCache);
     }
-  }, []);
+  });
+
+  const clearUserData = () => {
+    cache.delete("/api/users/" + userId);
+  };
 
   const userContextValue = {
     userData,
     updateUserData,
+    clearUserData,
   };
 
   return (
